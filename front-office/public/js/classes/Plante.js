3534cb5d9d5a93c0e3a15521c9f2d23c85c3339b
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-01-23 12:55:27 +0100

    Oubli add fichiers

--- front-office/public/js/classes/Plante.js
+++ front-office/public/js/classes/Plante.js
@@ -8,14 +8,55 @@ const db = mysql.createConnection({
 });
 
 class Plante {
-    constructor() {
+    constructor(datas) {
+        this.id = datas.id;
+        this.nom = datas.nom;
+        this.nom_latin = datas.nom_latin;
+        this.taille = datas.taille;
+        this.couleur = datas.couleur;
+        this.floraison = datas.floraison;
+        this.emplacement = datas.emplacement;
+        this.miellat = datas.miellat;
+        this.nectar = datas.nectar;
+        this.pollen = datas.pollen;
+        this.photo = datas.photo;
+        this.created_at = datas.created_at;
+        this.updated_at = datas.updated_at;
+        this.deleted_at = datas.deleted_at;
     }
 
     static all(){
-
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM plante";
+            db.query(sql, (error, result) => {
+                if (!error){
+                    let res = [];
+                    result.forEach(datas => {res.push(
+                        new Plante(datas)
+                    )});
+                    resolve(res)
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
     }
 
     static find(id){
-
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM plante WHERE id = ?";
+            db.query(sql, id, (error, result) => {
+                if (!error){
+                    const plante = new Plante(result[0]);
+                    resolve(plante)
+                }
+                else{
+                    reject(error)
+                }
+            })
+        })
     }
 }
+
+module.exports = Plante;
