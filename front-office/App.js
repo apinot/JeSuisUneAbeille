2f84ed6cf4bb48b3cb7fc8b09a2b08bbb717c4e6
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-01-23 12:55:27 +0100

    Oubli add fichiers

--- front-office/App.js
+++ front-office/App.js
@@ -1,9 +1,46 @@
+/**
+ * *************
+ * Requires
+ * *************
+ */
 const express = require('express');
+const Plante = require('./public/js/classes/Plante.js');
 
+/**
+ * *************
+ * Instantiations/Configs
+ * *************
+ */
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/public/views');
+app.use(express.static(process.cwd() + '/public'));
 
+/**
+ * *************
+ * Routing
+ * *************
+ */
+
+// Accueil
 app.get('/', (req, res) => {
-   res.send('Hello')
+    res.render('index')
+});
+
+app.get('/plantes', (req, res) => {
+   Plante.all()
+       .then(plantes => {
+           console.log(plantes);
+           res.json(plantes)
+       })
+       .catch(error => {
+           console.log(error);
+           res.render('error', error)
+       })
+});
+
+app.get('/scan', (req,res) => {
+    res.render('scan')
 });
 
 app.listen(3000);
