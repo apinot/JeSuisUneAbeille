3ca91011d6aee056f54208c956ab8d58b45afaed
apinot <antoine.pinot1@gmail.com>
2020-01-23 22:34:35 +0100

    meilleur code QR + bouton de telechargement

--- back-office/src/utils/QRCode.php
+++ back-office/src/utils/QRCode.php
@@ -3,11 +3,16 @@
 namespace abeille\utils;
 
 use chillerlan\QRCode\QRCode as CodeGenerator;
+use chillerlan\QRCode\QROptions;
 
 class QRCode {
     
     public static function generate(array $json) : string {
-        return (new CodeGenerator)->render(json_encode($json));
+        $options = new QROptions([
+            "eccLevel" => CodeGenerator::ECC_H,
+            "imageTransparent" => false,
+        ]);
+        return (new CodeGenerator($options))->render(json_encode($json));
     }
     
 }
